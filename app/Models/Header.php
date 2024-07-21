bc0a52a34567755ab853682c79bfcf11bfe0fd23
Syahril Haryono <syhrlhyn@gmail.com>
2024-07-21 12:43:02 +0700

    tes

--- app/Models/Header.php
+++ app/Models/Header.php
@@ -9,44 +9,25 @@ use Illuminate\Database\Eloquent\Model;
 class Header extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'title', 'name', 'image', 'image2'
     ];
-
-    /**
-     * Get the image attribute.
+/**
+     * image
      *
-     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     * @return Attribute
      */
     protected function image(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => $this->makeHttpsUrl($value),
+            get: fn ($value) => url('/storage/headers/' . $value),
         );
     }
 
-    /**
-     * Get the image2 attribute.
-     *
-     * @return \Illuminate\Database\Eloquent\Casts\Attribute
-     */
     protected function image2(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => $this->makeHttpsUrl($value),
+            get: fn ($value) => url('/storage/headers/' . $value),
         );
-    }
-
-    /**
-     * Make the URL use HTTPS.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function makeHttpsUrl($value): string
-    {
-        $url = url('/storage/headers/' . $value);
-        return str_replace('http://', 'https://', $url);
     }
 }
