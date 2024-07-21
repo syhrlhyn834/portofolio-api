b6970af7f38366c01e38b65986796f5565156273
Syahril Haryono <syhrlhyn@gmail.com>
2024-07-21 12:03:21 +0700

    Save local changes to package-lock.json

--- app/Models/Header.php
+++ app/Models/Header.php
@@ -9,25 +9,44 @@ use Illuminate\Database\Eloquent\Model;
 class Header extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'title', 'name', 'image', 'image2'
     ];
-/**
-     * image
+
+    /**
+     * Get the image attribute.
      *
-     * @return Attribute
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
      */
     protected function image(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => url('/storage/headers/' . $value),
+            get: fn ($value) => $this->makeHttpsUrl($value),
         );
     }
 
+    /**
+     * Get the image2 attribute.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
     protected function image2(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => url('/storage/headers/' . $value),
+            get: fn ($value) => $this->makeHttpsUrl($value),
         );
+    }
+
+    /**
+     * Make the URL use HTTPS.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function makeHttpsUrl($value): string
+    {
+        $url = url('/storage/headers/' . $value);
+        return str_replace('http://', 'https://', $url);
     }
 }
